4950821bc019295ae67782da1cabe6631530f8d5
Alexander Zyulikov <azyulikov@icloud.com>
2021-05-14 00:17:08 +0600

    fix

--- src/ng2-select2.module.ts
+++ src/ng2-select2.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 
 export { Select2OptionData, Select2TemplateFunction } from './ng2-select2.interface';
 import { Select2Component } from './ng2-select2.component';
+import { CommonModule } from '@angular/common';
 
 export { Select2Component } from './ng2-select2.component';
 
 @NgModule({
+    imports: [CommonModule],
     declarations: [Select2Component],
     exports: [Select2Component]
 })
